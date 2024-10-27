536abc2a0e68a08dc7d03ba2a88b7866a6580fb7
pySam1459 <pysam1459@gmail.com>
2024-10-27 17:38:29 +0000

    COMMIT #2 1) region tool now supports youtube video 2) simplified multiple capture config, added new CaptureCommon BaseModel

--- src/config/analyser_cfg.py
+++ src/config/analyser_cfg.py
@@ -1,8 +1,6 @@
-import os
 from collections import Counter
 from functools import partial
 from pathlib import Path
-from re import match
 from pydantic import BaseModel, ConfigDict, Field, field_validator, computed_field, model_validator
 from typing import Any, Optional, Self, Type
 
@@ -12,8 +10,9 @@ from utils import Timestamp, load_file, recursive_union, gen_default_name, GameT
 from utils.enums import GameType, CaptureMode, IGNMatrixMode, Team, SaveFileType
 from utils.constants import *
 
+from .common import CaptureCommon
 from .region_models import TimerRegion, KFLineRegion
-from .utils import validate_config, VALID_URLS
+from .utils import validate_config
 
 
 __all__ = [
@@ -67,58 +66,10 @@ class RegionsParser(TimerRegion, KFLineRegion):
     model_config = ConfigDict(extra="allow")
 
 
-class CaptureParser(BaseModel):
-    mode:    CaptureMode 
+class CaptureParser(CaptureCommon):
     regions: RegionsParser
 
-    file:    Optional[Path]      = None
-    url:     Optional[str]       = None
-    offset:  Optional[Timestamp] = None
-
     model_config = ConfigDict(extra="forbid")
-
-    @field_validator("offset", mode="before")
-    @classmethod
-    def parse_offset(cls, v: Any) -> Optional[Timestamp]:
-        if v is None:
-            return None
-        if isinstance(v, int) and v >= 0:
-            return Timestamp.from_int(v)
-        elif isinstance(v, str) and match(TIMESTAMP_PATTERN, v):
-            return Timestamp.from_str(v)
-
-        raise ValueError(f"Cannot parse capture offset: {v}")
-
-
-    @model_validator(mode="after")
-    def file_exists(self) -> Self:
-        if self.mode == CaptureMode.VIDEOFILE:
-            if self.file is None:
-                raise ValueError(f"videofile capture mode requires `file` field to be specified")
-            elif not self.file.exists():
-                raise ValueError(f"Video file {self.file} does not exist")
-            elif not os.access(self.file, os.R_OK):
-                raise ValueError(f"Permission Error: Invalid permissions to read video file {self.file}")
-        return self
-
-    @model_validator(mode="after")
-    def validate_url(self) -> Self:
-        if self.mode not in [CaptureMode.YOUTUBE, CaptureMode.TWITCH]:
-            return self
-
-        if self.url is None:
-            raise ValueError(f"{self.mode.value} capture mode required `url` field to be specified")
-
-        if self.mode not in VALID_URLS:
-            return self
-
-        valid_urls = VALID_URLS[self.mode]
-        for vurl in valid_urls:
-            if self.url.startswith(vurl):
-                return self
-        else:
-            valid_urls_str = ", ".join(valid_urls)
-            raise ValueError(f"Invalid {self.mode.value} URL: {self.url}\nIt must start with {valid_urls_str}")
 
 
 class SaveParser(BaseModel):
--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,8 +1,10 @@
+import cv2
+from capture.youtube import get_video_stream_details
 from config import RTConfig
 from utils.cli import AnalyserArgs
 from utils.enums import CaptureMode
 
-from .regiontool import RegionTool, RTScreenShot, RTVideoFile
+from .regiontool import RegionTool, RTScreenShot, RTVideoFile, RTYoutubeVideo
 from .deps_check import check_dependencies
 
 
@@ -10,6 +12,20 @@ __all__ = [
     "create_regiontool",
     "check_dependencies"
 ]
+
+def create_youtube_rt(config: RTConfig) -> RegionTool:
+    assert config.capture.url is not None, "Invalid Config! YouTube URL is not provided! Please add YouTube URL to capture.url"
+
+    video_stream_url = get_video_stream_details(config.capture.url)
+
+    cap = cv2.VideoCapture(video_stream_url.url)
+    if not cap.isOpened():
+        raise ValueError(f"Video Capture Error: Unable to open video stream from {config.capture.url}")
+
+    if video_stream_url.is_live:
+        raise NotImplementedError(f"RegionTool does not support Youtube livestream yet")
+    else:
+        return RTYoutubeVideo(config, cap)
 
 
 def create_regiontool(args: AnalyserArgs, config: RTConfig) -> RegionTool:
@@ -19,5 +35,7 @@ def create_regiontool(args: AnalyserArgs, config: RTConfig) -> RegionTool:
             return RTScreenShot(args, config)
         case CaptureMode.VIDEOFILE:
             return RTVideoFile(config)
+        case CaptureMode.YOUTUBE:
+            return create_youtube_rt(config)
         case _:
             raise NotImplementedError(f"RegionTool does not support {mode} yet")
